094fa5895b29ab38329c84bd57c821f9ba3f23e9
Dan Reist <djreist@outlook.com>
2021-08-03 13:41:09 -0400

    Discovery fully working

--- Nintex.Workflow.SharePoint/Nintex.Workflow.SharePoint/Scripts/Invoke-NintexWorkflowSharePointDiscovery.ps1
+++ Nintex.Workflow.SharePoint/Nintex.Workflow.SharePoint/Scripts/Invoke-NintexWorkflowSharePointDiscovery.ps1
@@ -72,8 +72,20 @@ $whoami = whoami
 $message = "`nScript is starting.`nRunning As: $whoami`n$parameterString"
 Write-OperationsManagerEvent @writeOperationsManagerEventParams -Severity 0 -Description $message -DebugLogging:$debug
 
+# Define a lookup table to relate property names to the class instance
+$propertyInstance = @{
+    Farm = '$MPElement[Name="Nintex.Workflow.SharePoint.Class"]/Farm$'
+    NintexVersion = '$MPElement[Name="Nintex.Workflow.SharePoint.Class"]/Version$'
+    InstanceName = '$MPElement[Name="Nintex.Workflow.SharePoint.Database.Class"]/InstanceName$'
+    DatabaseName = '$MPElement[Name="Nintex.Workflow.SharePoint.Database.Class"]/DatabaseName$'
+    Version = '$MPElement[Name="Nintex.Workflow.SharePoint.Database.Class"]/Version$'
+}
+
+# Initialize the DiscoveryData variable
+$discoveryData = @()
 #endregion initialize script
 
+#region Get Nintex Installation Info
 try
 {
     Add-PSSnapin -Name Microsoft.SharePoint.PowerShell -ErrorAction Stop
@@ -84,6 +96,8 @@ catch
     Write-OperationsManagerEvent @writeOperationsManagerEventParams -Severity 1 -Description $message -DebugLogging
     exit
 }
+
+$farm = Get-SPFarm
 
 try
 {
@@ -98,6 +112,33 @@ catch
 
 $message = "`nNWAdmin.exe Path: $($nwAdminExe.Source)"
 Write-OperationsManagerEvent @writeOperationsManagerEventParams -Severity 0 -Description $message -DebugLogging:$debug
+
+[System.Reflection.Assembly]::Load('Nintex.Workflow, Version=1.0.0.0, Culture=neutral, PublicKeyToken=913f6bae0ca5ae12') > $null
+$nintexVersion = [Nintex.Workflow.Version]::GetVersion()
+$message = "`nNintex Version: $($nintexVersion.ToString())"
+Write-OperationsManagerEvent @writeOperationsManagerEventParams -Severity 0 -Description $message -DebugLogging:$debug
+
+$discoveryData += @{
+    DiscoveryType = '$MPElement[Name="Nintex.Workflow.SharePoint.Class"]$'
+    Properties = @(
+        @{
+            ClassName = 'Nintex.Workflow.SharePoint.Class'
+            PropertyInstance = $propertyInstance.Farm
+            PropertyName = 'Farm'
+            PropertyValue = $farm.Name
+        }
+        @{
+            ClassName = 'Nintex.Workflow.SharePoint.Class'
+            PropertyInstance = $propertyInstance.NintexVersion
+            PropertyName = 'Version'
+            PropertyValue = $nintexVersion.ToString()
+        }
+    )
+}
+
+#endregion Get Nintex Installation Info
+
+#region Get Nintex Database Info
 
 # Get the Nintex Workflow databases
 $databasesRaw = ( & $nwAdminExe.Source -o CheckDatabaseVersion ) -join "`n"
@@ -114,17 +155,87 @@ if ( $databasesRaw -match 'Command line execution error: Failed to open a connec
 
 # Parse the string into an object
 $databasesString = $databasesRaw -split "`n`n"
-$databases = @()
 foreach ( $databaseString in $databasesString )
 {
     $databaseProperties = $databaseString -split "`n"
-    $databases += New-Object -TypeName PSCustomObject -Property @{
-        ConnectionString = $databaseProperties[0]
-        Type = $databaseProperties[1].Replace('Type: ','')
-        Version = [System.Version]::new($databaseProperties[2].Replace('Version: ',''))
-        Comment = $databaseProperties[3]
+    $databaseConnectionString = New-Object -TypeName System.Data.SqlClient.SqlConnectionStringBuilder -ArgumentList $databaseProperties[0]
+    $databaseType = $databaseProperties[1].Replace('Type: ','')
+    $databaseVersion = [System.Version]::new($databaseProperties[2].Replace('Version: ',''))
+    $databaseStatus = $databaseProperties[3]
+
+     switch ( $databaseType )
+     {
+         Configuration { $discoveryType = '$MPElement[Name="Nintex.Workflow.SharePoint.Database.Configuration.Class"]$' }
+         Content { $discoveryType = '$MPElement[Name="Nintex.Workflow.SharePoint.Database.Content.Class"]$' }
+     }
+
+     $discoveryData += @{
+         DiscoveryType = $discoveryType
+         Properties = @(
+            @{
+                ClassName = 'Nintex.Workflow.SharePoint.Class'
+                PropertyInstance = $propertyInstance.Farm
+                PropertyName = 'Farm'
+                PropertyValue = $farm.Name
+             }    
+            @{
+                ClassName = 'Nintex.Workflow.SharePoint.Database.Class'
+                PropertyInstance = $propertyInstance.InstanceName
+                PropertyName = 'InstanceName'
+                PropertyValue = $databaseConnectionString.DataSource
+             }
+             @{
+                ClassName = 'Nintex.Workflow.SharePoint.Database.Class'
+                PropertyInstance = $propertyInstance.DatabaseName
+                PropertyName = 'DatabaseName'
+                PropertyValue = $databaseConnectionString.InitialCatalog
+             }
+             @{
+                ClassName = 'Nintex.Workflow.SharePoint.Database.Class'
+                PropertyInstance = $propertyInstance.Version
+                PropertyName = 'Version'
+                PropertyValue = $databaseVersion.ToString()
+             }
+         )
+     }
+}
+
+#endregion Get Nintex Database Info
+
+#region Create Discovery Data
+
+if (-not $TestRun)
+{
+    Write-Debug -Message 'Generating discovery object to return to SCOM'
+
+    # Initialize SCOM discovery data object
+    $scomDiscoveryData = $momapi.CreateDiscoveryData(0, $SourceId, $ManagedEntityId)
+
+    # Create an instance of the Nintex Workflow for SharePoint installation to create a relationship with the databases
+    #$installationInstance = $scomDiscoveryData.CreateClassInstance('$MPElement[Name="Nintex.Workflow.SharePoint.Class"]$')
+
+    foreach ( $currentDiscoveryData in $discoveryData )
+    {
+        # Only return mapped discovery types
+        if ($currentDiscoveryData.DiscoveryType -ne 'Unknown')
+        {
+            Write-Debug -Message "Discovery class --> $($currentDiscoveryData.DiscoveryType)"
+
+            # Initialize the appropriate class
+            $discoveredObject = $scomDiscoveryData.CreateClassInstance($currentDiscoveryData.DiscoveryType)
+
+            # Add all additional properties
+            foreach ($property in $currentDiscoveryData.Properties)
+            {
+                Write-Debug -Message "Adding property '$($property.PropertyName) with value $($property.PropertyValue)"
+                $discoveredObject.AddProperty($property.PropertyInstance, $property.PropertyValue)
+            }
+
+            $scomDiscoveryData.AddInstance($discoveredObject)
+        }
     }
 }
+#endregion Create Discovery Data
 
 # Log an event for script ending and total execution time.
 $endTime = Get-Date
@@ -132,3 +243,38 @@ $scriptTime = ($endTime - $startTime).TotalSeconds
 
 $message = "`n Script Completed. `n Script Runtime: ($scriptTime) seconds."
 Write-OperationsManagerEvent @writeOperationsManagerEventParams -Severity 0 -Description $message -DebugLogging:$debug
+
+$discoveredString = New-Object -TypeName System.Text.StringBuilder
+$d = 1
+foreach ( $discovered in $discoveryData )
+{
+    $discoveredString.AppendLine("Discovered Item $d") > $null
+    $discoveredString.AppendLine("    - Discovery Type: $($discovered.DiscoveryType)") > $null
+    $discoveredString.AppendLine("    - Properties:") > $null
+
+    $p = 1
+    foreach ( $property in $discovered.Properties )
+    {
+        $discoveredString.AppendLine("        + Property $p") > $null
+        $discoveredString.AppendLine("            > ClassName: $($property.ClassName)") > $null
+        $discoveredString.AppendLine("            > PropertyInstance: $($property.PropertyInstance)") > $null
+        $discoveredString.AppendLine("            > PropertyName: $($property.PropertyName)") > $null
+        $discoveredString.AppendLine("            > PropertyValue: $($property.PropertyValue)") > $null
+        $discoveredString.AppendLine('') > $null
+        $p++
+    }
+
+    $discoveredString.AppendLine('') > $null
+    $d++
+}
+Write-OperationsManagerEvent @writeOperationsManagerEventParams -Severity 0 -Description $discoveredString.ToString() -DebugLogging:$debug
+
+if ($TestRun)
+{
+    # Return the object containing the discovered information
+    return $discoveryData
+}
+else
+{
+    $scomDiscoveryData
+}
